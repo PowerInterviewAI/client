5401820089b337c617688015cd70dfd1e79c5354
alpha dev <alpha5611331@gmail.com>
2025-11-18 15:36:48 +0100

    fixed audio listing

--- backend/services/audio_service.py
+++ backend/services/audio_service.py
@@ -4,16 +4,12 @@ import pyaudiowpatch as pyaudio
 
 
 class AudioService:
+    _pa = pyaudio.PyAudio()
+
     @classmethod
     def get_input_devices(cls) -> list[dict[str, Any]]:
-        pa = pyaudio.PyAudio()
-        ret = [device for device in pa.get_device_info_generator() if device["maxInputChannels"] > 0]
-        pa.terminate()
-        return ret
+        return [device for device in cls._pa.get_device_info_generator() if device["maxInputChannels"] > 0]
 
     @classmethod
     def get_output_devices(cls) -> list[dict[str, Any]]:
-        pa = pyaudio.PyAudio()
-        ret = [device for device in pa.get_device_info_generator() if device["maxOutputChannels"] > 0]
-        pa.terminate()
-        return ret
+        return [device for device in cls._pa.get_device_info_generator() if device["maxOutputChannels"] > 0]
