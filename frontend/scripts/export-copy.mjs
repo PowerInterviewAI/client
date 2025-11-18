c300c60cfe12dfaa03d2f950c95c14163fd877ea
alpha dev <alpha5611331@gmail.com>
2025-11-18 09:23:37 +0100

    added export feature

---
+++ frontend/scripts/export-copy.mjs
@@ -0,0 +1,25 @@
+import fs from "fs";
+import path from "path";
+
+const src = path.resolve("out");
+const dest = path.resolve("../backend/public");
+
+function copyRecursive(srcDir, destDir) {
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir, { recursive: true });
+  }
+
+  for (const item of fs.readdirSync(srcDir)) {
+    const srcPath = path.join(srcDir, item);
+    const destPath = path.join(destDir, item);
+
+    if (fs.lstatSync(srcPath).isDirectory()) {
+      copyRecursive(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyRecursive(src, dest);
+console.log("Export copied to backend/public");
