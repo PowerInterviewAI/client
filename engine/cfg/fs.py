853f5f8fe4c55b5230c9bf4799aaa4ad2441a97a
alpha dev <alpha5611331@gmail.com>
2025-11-25 20:44:23 +0100

    fixed fs configuration error

--- engine/cfg/fs.py
+++ engine/cfg/fs.py
@@ -3,10 +3,11 @@ from pathlib import Path
 
 class Config:
     ROOT_DIR: Path = Path(__file__).parent.parent.parent
-    APP_DIR: Path = ROOT_DIR / "app"
+
+    ENGINE_DIR: Path = Path(__file__).parent.parent
     MODELS_DIR: Path = ROOT_DIR / "models"
 
-    PUBLIC_DIR: Path = APP_DIR / "public"
+    PUBLIC_DIR: Path = ENGINE_DIR / "public"
 
     CONFIG_FILE: Path = ROOT_DIR / "config.json"
 
