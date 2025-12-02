fb11f44a84d12e3eece75314730590c68d4963e8
alpha dev <alpha5611331@gmail.com>
2025-12-02 21:23:54 +0100

    eliminated model dir

--- engine/cfg/fs.py
+++ engine/cfg/fs.py
@@ -5,7 +5,6 @@ class Config:
     ROOT_DIR: Path = Path(__file__).parent.parent.parent
 
     ENGINE_DIR: Path = Path(__file__).parent.parent
-    MODELS_DIR: Path = ROOT_DIR / "models"
 
     PUBLIC_DIR: Path = ENGINE_DIR / "public"
 
