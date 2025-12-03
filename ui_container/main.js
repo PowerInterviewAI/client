21b05fe2b6487534d248be62cfa0948e8535a189
alpha dev <alpha5611331@gmail.com>
2025-12-03 21:52:04 +0100

    set window title

--- ui_container/main.js
+++ ui_container/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
+        title: 'Power Interview',
         width: 800,
         height: 600,
         webPreferences: {
