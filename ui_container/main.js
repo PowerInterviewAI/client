bca905472da1c5e7e9abffc2ac6f8116f1fe7073
alpha dev <alpha5611331@gmail.com>
2025-12-03 21:55:53 +0100

    saved last position

--- ui_container/main.js
+++ ui_container/main.js
@@ -1,16 +1,27 @@
 const { app, BrowserWindow } = require('electron');
+const Store = require('electron-store').default;
+
+const store = new Store();
 
 function createWindow() {
+    const savedBounds = store.get('windowBounds') || { width: 800, height: 600 };
+
     const win = new BrowserWindow({
         title: 'Power Interview',
-        width: 800,
-        height: 600,
+        x: savedBounds.x,
+        y: savedBounds.y,
+        width: savedBounds.width,
+        height: savedBounds.height,
         webPreferences: {
             preload: `${__dirname}/preload.js`
         }
     });
 
-    // Load App backend
+    // Save window size & position when user closes the window
+    win.on('close', () => {
+        store.set('windowBounds', win.getBounds());
+    });
+
     win.loadURL('http://localhost:8080');
 }
 
