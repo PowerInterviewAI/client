fd01bfdf40c2b3bc91a67f3071ae83a651241173
alpha dev <alpha5611331@gmail.com>
2025-12-10 16:53:44 +0100

    hide video panel when video control is disabled

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -137,7 +137,7 @@ export default function Home() {
         {/* Left Column: Video + Transcription */}
         <div className="flex flex-col gap-2 w-1/2 md:w-96 shrink-0 min-h-0">
           {/* Video Panel - Small and compact */}
-          <div className="h-48 shrink-0">
+          <div className="h-48 shrink-0" hidden={!config?.enable_video_control}>
             <VideoPanel
               ref={videoPanelRef}
               runningState={
