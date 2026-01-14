0d05ad02c03a9a0f8c3fbe92b726b9b9837bcab9
alpha dev <alpha5611331@gmail.com>
2026-01-14 20:06:57 +0100

    fix: enhance MainFrame component with dynamic stealth mode styling and improved layout

--- ui/components/main-frame.tsx
+++ ui/components/main-frame.tsx
@@ -1,10 +1,14 @@
+'use client';
+
+import useIsStealthMode from '@/hooks/use-is-stealth-mode';
 import Titlebar from './titlebar';
 import WindowResizer from './window-resizer';
 
 export default function MainFrame({ children }: { children: React.ReactNode }) {
+  const isStealthMode = useIsStealthMode();
   return (
-    <div className="overflow-hidden rounded-xl border">
-      <div className="flex flex-col h-screen">
+    <div className={`overflow-hidden ${isStealthMode ? 'rounded-xl' : ''}  border shadow-2xl`}>
+      <div className="flex flex-col h-[calc(100vh-2px)]">
         <Titlebar />
         <div className="flex-1 flex flex-col overflow-auto">{children}</div>
       </div>
